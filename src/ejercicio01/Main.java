6b24ad98905ccea8557b2197aafabc79c3ae6aea
javier Almendras <javiermax26@gmail.com>
2025-12-04 18:47:18 -0300

    Agregados al Ejercicio 01  del TP05 iES6 Javier Almendras

--- src/ejercicio01/Main.java
+++ src/ejercicio01/Main.java
@@ -1,5 +1,19 @@
 package ejercicio01;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+    // linea de comentario
+    // datos primitivos
+    // declarar variables e inicianilizar
+    int numeroA = 3; 
+    double numeroB = 5.2;
+    boolean bandera = true;
+    char caracter = '$';
 
+    String mensaje01 = "las variables son: ";
+    String mensaje02 = " que es un numero entero: ";
+    double longitudDeLaCadena = mensaje01.length();
+
+    System.out.println(mensaje01+numeroA+mensaje02+" y el tamaño del mensaje es:"+longitudDeLaCadena);
+    }
 }
