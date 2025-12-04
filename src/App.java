e8231b11323dd2bbb036e21a88d953737df84f72
javier Almendras <javiermax26@gmail.com>
2025-12-04 16:18:24 -0300

    inicializacion del TP05 IES Javier Almnedras

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // java es fuertemente TIPADO
+        System.out.println("Hello, IES 6 INFORMATICA!");
+
+        // definicion e inicializacio
+        int numeroA = 10;
+        int numeroB = 5; 
+
+        numeroA = 30;
+
+        
+    
+        System.out.println("la SUMA de los numeros es: " + (numeroA + numeroB));
+    }
+}
